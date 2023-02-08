8f49971f5b449ddfd695fb01c8e2d2f3f5f91e3a
orin-sightfactory <63678490+orin-sightfactory@users.noreply.github.com>
2023-02-08 11:21:44 -0400

    Update menus.php

--- admin/includes/menus.php
+++ admin/includes/menus.php
@@ -3,15 +3,12 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
 
-global $vwp_menu_items , $vwp_post_id;
+global $vwpwcc_menu_items , $vwpwcc_post_id;
 
 
-/*$description_fields = array(
-    array("label" => "Description", "key" => "description" ,"type" => "textarea", "attr_maxlength" => 500 , "required" => false , "layout" => "col-12" , "notes" => ""),
-);*/
 
 
-$vwp_menu_items = array(
+$vwpwcc_menu_items = array(
     array("key" => "conditions" , "label" => "Conditions" , "title" => "Conditions" , "type" => "settings" , "class" => "conditions" , "callback"=> vwpwcc_get_conditions()),
     array("key" => "description" , "label" => "Description" , "title" => "Description" , "type" => "dropdown" , "class" => "wcc-description", "callback"=> vwpwcc_get_description()),
     array("key" => "slug" , "label" => "Slug" , "title" => "Slug" , "type" => "dropdown" , "class" => "slug" ,  "callback"=> vwpwcc_get_slug()),
@@ -21,16 +18,14 @@ $vwp_menu_items = array(
 
 
 function vwpwc_populate_menu(){
-    global $vwp_menu_items;
+    global $vwpwcc_menu_items;
 
     $vwp_menu = "";
-    foreach($vwp_menu_items as $menu_item){
+    foreach($vwpwcc_menu_items as $menu_item){
         $vwp_menu .= '<button class="vwp-menu-item '.$menu_item["key"].' '.$menu_item["type"].'" data-key="'.$menu_item["key"].'">'.$menu_item["label"].'</button>';
-        //add_vwp_widget($menu_item["key"] , $menu_item["Shortcode"] , $menu_item["fields"]); 
     }
 
 
-    //$vwp_menu_widgets = vwp_populate_menu_widgets($vwp_menu_items);
 
 
     return $vwp_menu;
@@ -38,9 +33,9 @@ function vwpwc_populate_menu(){
 
 //add_vwp_menu(
 function vwpwc_populate_menu_widgets($widget_type){
-    global $vwp_menu_items;
+    global $vwpwcc_menu_items;
     $vwp_menu_widgets = "";
-    foreach($vwp_menu_items as $widget_item){
+    foreach($vwpwcc_menu_items as $widget_item){
         if($widget_item["type"] == $widget_type)
         $vwp_menu_widgets .= '<div class="vwp-menu-widget '.$widget_item["key"].' '.$widget_item["type"].'"><div class="vwp-widget-title">'.$widget_item["title"].'</div>'.$widget_item["callback"].'</div>';
     }
@@ -51,10 +46,10 @@ function vwpwc_populate_menu_widgets($widget_type){
 
 
 function vwpwcc_get_description(){
-    global $vwp_post_id;
+    global $vwpwcc_post_id;
     ob_start();
     ?>
-    <textarea id="vwpwcc-collection-description" type="text" name="vwpwcc-collection-description"><?php echo @get_the_excerpt($vwp_post_id)  ?></textarea>  
+    <textarea id="vwpwcc-collection-description" type="text" name="vwpwcc-collection-description"><?php echo esc_html(@get_the_excerpt($vwpwcc_post_id))  ?></textarea>  
     <hr class="vwp-divider" />
     <button class="vwp-button vwp-button-save-description">Done</button>
 
@@ -68,10 +63,10 @@ function vwpwcc_get_description(){
 }
 
 function vwpwcc_get_slug(){
-    global $vwp_post_id;
+    global $vwpwcc_post_id;
     ob_start();
     ?>
-    <input type="text" id="vwpwcc-collection-slug" name="vwpwcc-collection-slug" value="<?php echo get_post_field('post_name', $vwp_post_id); ?>" />
+    <input type="text" id="vwpwcc-collection-slug" name="vwpwcc-collection-slug" value="<?php echo esc_html(get_post_field('post_name', $vwpwcc_post_id)); ?>" />
     <hr class="vwp-divider" />
     <button class="vwp-button vwp-button-save-slug">Done</button>
 
@@ -86,10 +81,10 @@ function vwpwcc_get_slug(){
 
 
 function vwpwcc_get_shortcode(){
-    global $vwp_post_id;
+    global $vwpwcc_post_id;
     ob_start();
     ?>
-    <div id="wc-shortcode" class="vwp-view-action vwp-form-row"><div id="vwpwccol-shortcode" data-id="<?php echo $vwp_post_id ?>">[collections id="<?php echo $vwp_post_id ?>"]</div></div>  
+    <div id="wc-shortcode" class="vwp-view-action vwp-form-row"><div id="vwpwccol-shortcode" data-id="<?php echo $vwpwcc_post_id ?>">[collections id="<?php echo intval($vwpwcc_post_id) ?>"]</div></div>  
     <hr class="vwp-divider" />
     <button class="vwp-button vwp-button-copy-shortcode">Copy</button>
     <?php
@@ -104,9 +99,9 @@ function vwpwcc_get_shortcode(){
 
 
 function vwpwcc_get_conditions(){
-    global $vwp_post_id;
+    global $vwpwcc_post_id;
     
-    $collection_settings = get_post_meta( $vwp_post_id, "collection_settings" ,true);
+    $collection_settings = get_post_meta( $vwpwcc_post_id, "collection_settings" ,true);
     ob_start();
     ?>
     <div id="term-autosuggest-widget" class="autosuggest-widget"><div id="autosuggest-search-wrapper"><input type="text" id="term-autosuggest-input" placeholder="Search..." name="term-autosuggest-input" /></div><div id="term-autosuggest-list"></div></div>
@@ -114,9 +109,8 @@ function vwpwcc_get_conditions(){
         <div class="vwp-form-row label-inline"><input type="radio" id="all" name="condition-match" value="all" checked="checked" /><label class="vwpvf-label condition-match" for="all">All Conditions</label><input type="radio" id="any" name="condition-match" value="any" /><label class="vwpvf-label condition-match" for="any">Any Condition</label></div>
         <div class="vwpvf-repeatable-group"><!--span class="vwpwcc-loading">Loading conditions...</span--></div>
         <button id="vwpwccol-add-condition" class="vwp-btn">Add</button>
-        <!--button id="vwpwccol-preview-products" class="vwp-btn">Preview</button-->
-        <div><textarea id="vwpwccol-conditions-json" name="vwpwccol-conditions-json"><?php echo @$collection_settings ?></textarea></div>
-        <input id="post-id" type="hidden" name="post_id" value="<?php echo @intval($vwp_post_id) ?>" />
+        <div><textarea id="vwpwccol-conditions-json" name="vwpwccol-conditions-json"><?php echo esc_html(@$collection_settings) ?></textarea></div>
+        <input id="post-id" type="hidden" name="post_id" value="<?php echo @intval($vwpwcc_post_id) ?>" />
     </div>
     <?php
     $conditions = ob_get_contents();
